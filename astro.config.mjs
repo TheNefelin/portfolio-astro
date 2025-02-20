d522b60d4b680fb89dbacbc4c0838d2ed6349bfe
Francisco Carmona <flcarmonac@yahoo.com>
2025-02-20 18:04:07 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
-import vercel from '@astrojs/vercel'; // Importar el adaptador de Vercel
+// import vercel from '@astrojs/vercel'; // Importar el adaptador de Vercel
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +10,6 @@ export default defineConfig({
       tailwindcss(),
     ],
   },
-  output: 'server', // Habilitar SSR
-  adapter: vercel(), // Usar el adaptador de Vercel
+  // output: 'server', // Habilitar SSR
+  // adapter: vercel(), // Usar el adaptador de Vercel
 });
